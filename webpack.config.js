56212dc43c510ba2118f618411822c46ba29e7e4
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-16 16:55:33 +0700

    Configured project

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.pug',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.html'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: ['html-loader?attrs=false', 'pug-html-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader']
+                    })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {
+                            loader: "css-loader"
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                includePaths: ['node_modules/normalize-scss/sass'],
+                                data: `
+                                @import 'src/scss/scss-assets/_variables.scss';
+                                @import 'src/scss/scss-assets/_mixins.scss';
+                                @import 'src/scss/scss-assets/_functions.scss';
+                                @import 'src/scss/scss-assets/_config.scss';
+                                `
+                            }
+                        }
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './dist/common.css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.pug',
+            inject: false
+        })
+    ]
+};
