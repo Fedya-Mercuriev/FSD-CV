40b7f4b8944fc4e667283767761b80ef2c63512b
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-03-18 18:05:24 +0700

    Fixed webpack configs (works in dev and prod)

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,61 @@
+'use strict';
+
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.pug',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.html'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.pug$/,
-                use: ['html-loader?attrs=false', 'pug-html-loader']
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract(
-                    {
-                        fallback: 'style-loader',
-                        use: ['css-loader']
-                    })
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: "css-loader"
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            includePaths: ['node_modules/normalize-scss/sass'],
-                            data: `
+module.exports = (env, argv) => {
+
+    let config = {
+        entry: './src/app.js',
+        output: {
+            filename: 'build.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.pug$/,
+                    use: ['html-loader?attrs=false', 'pug-html-loader']
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: argv.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader
+                        },
+                        {
+                            loader: "css-loader"
+                        },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                includePaths: ['node_modules/normalize-scss/sass'],
+                                data: `
                                  @import './src/scss/scss-assets/_variables.scss';
                                  @import './src/scss/scss-assets/_mixins.scss';
                                  @import './src/scss/scss-assets/_functions.scss';
                                  @import './src/scss/scss-assets/_config.scss';
                                  `
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        devtool: 'source-map',
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'common.css'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: 'src/index.pug',
+                inject: false
+            })
         ]
-    },
-    plugins: [
-        new ExtractTextPlugin({
-            filename: './dist/common.css'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.pug',
-            inject: false
-        })
-    ]
+    };
+
+    return config;
 };
